f96f1298a254920346e743a90b269364322e3c87
Blake Watters <blake@near-time.com>
2008-11-04 18:29:17 -0500

    support for statefully loading article via an authenticated session

--- lib/reddit/session.rb
+++ lib/reddit/session.rb
@@ -15,12 +15,14 @@ module Reddit
   
   # A reddit browsing session.
   class Session
+    attr_reader :cookie
     
     # initialize the session with a username and password.  Currently not used.
     def initialize(username = "", password = "")
       @username = username
       @password = password
       @logged_in = false
+      @cookie = ''
     end
     
     # return the main reddit.
@@ -49,9 +51,23 @@ module Reddit
       resources = JSON.parse(result.body, :max_nesting => 0)
       if resources['error']
         raise AuthenticationException, resources['error']['message']
+      else
+        @logged_in = true
+        @cookie = result.response['set-cookie']
       end
       pp resources
       return true
+    end
+    
+    def get_article(url)
+      api_url = "http://www.reddit.com/api/info.json?count=1&url=#{url}"
+      uri = URI.parse(api_url)
+      headers = { 'Cookie' => self.cookie }
+      http = Net::HTTP.new(uri.host, uri.port)
+      response = http.get(uri.path + "?#{uri.query}", headers)
+      info = JSON.parse(response.body, :max_nesting => 0)
+      pp info
+      Article.new(info['data']['children'][0]['data'])
     end
   end
 end
