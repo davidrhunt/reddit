eb6aa2303c50c7c03a5e2f6ef6addb1cdcfd0099
Blake Watters <blake@near-time.com>
2008-11-04 20:05:05 -0500

    refactored the API a bit. still need to decode the hashing issues to be able to push votes

--- lib/reddit/session.rb
+++ lib/reddit/session.rb
@@ -4,6 +4,10 @@ module Reddit
   SUBREDDIT_URL = BASE_URL + "r/[subreddit]/"
   COMMENTS_URL = BASE_URL + "comments/[id]/"
   
+  LOGIN_URL = BASE_URL + "api/login.json"
+  INFO_URL = BASE_URL + "api/info.json?count=1&url=[url]"
+  VOTE_URL = BASE_URL + "api/vote.json"
+  
   # raised when attempting to interact with a subreddit that doesn't exist.
   class SubredditNotFound < StandardError; end
   
@@ -15,7 +19,13 @@ module Reddit
   
   # A reddit browsing session.
   class Session
-    attr_reader :cookie
+    attr_reader :username, :cookie
+    
+    def self.login(username, password)
+      session = Session.new(username, password)
+      session.authenticate
+      session
+    end
     
     # initialize the session with a username and password.  Currently not used.
     def initialize(username = "", password = "")
@@ -40,12 +50,16 @@ module Reddit
       return User.new(username)
     end
     
+    def owner
+      User.new(@username)
+    end
+    
     def logged_in?
       @logged_in
     end
     
-    def login
-      url = "http://www.reddit.com/api/login.json"
+    def authenticate
+      url = LOGIN_URL
       params = { 'reason' => '', 'op' => 'login-main', 'dest' => '/', 'user_login' => @username, 'passwd_login' => @password, 'rem' => '1' }
       result = Net::HTTP.post_form(URI.parse(url), params)
       resources = JSON.parse(result.body, :max_nesting => 0)
@@ -60,14 +74,50 @@ module Reddit
     end
     
     def get_article(url)
-      api_url = "http://www.reddit.com/api/info.json?count=1&url=#{url}"
-      uri = URI.parse(api_url)
-      headers = { 'Cookie' => self.cookie }
+      url = INFO_URL.gsub('[url]', url)
+      info = get(url)
+      Article.new(info['data']['children'][0]['data'])
+    end
+    
+    def like!(object)
+      vote(object, 1)
+    end
+    
+    def dislike!(object)
+      vote(object, -1)
+    end
+    
+    def save!(object)
+      raise "Not Yet Implemented"
+    end
+    
+    protected
+    def vote(object, direction)
+      params = { 'dir' => direction, 'id' => object.id, 'r' => 'reddit.com', 'uh' => '', 'vh' => '' }
+      debugger
+      info = post(VOTE_URL, params)
+      pp info
+      debugger
+      info
+    end
+    
+    def headers
+      { 'Cookie' => self.cookie }
+    end
+    
+    def get(url)
+      uri = URI.parse(url)
       http = Net::HTTP.new(uri.host, uri.port)
       response = http.get(uri.path + "?#{uri.query}", headers)
-      info = JSON.parse(response.body, :max_nesting => 0)
-      pp info
-      Article.new(info['data']['children'][0]['data'])
+      JSON.parse(response.body, :max_nesting => 0)
+    end
+    
+    def post(url, params = {})
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      params_string = params.map { |k, v| "#{k}=#{v}" }.join('&')
+      response = http.post(uri.path, params_string, headers)
+      JSON.parse(response.body, :max_nesting => 0)
     end
   end
 end
